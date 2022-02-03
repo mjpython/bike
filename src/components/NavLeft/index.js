ca497f4f3e164ca9b13d2490cc00f5ebf121f7a5
Majie <714544413@qq.com>
2022-02-03 23:30:19 +0800

    主页头部时间完成

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,7 +1,34 @@
 import React, { PureComponent } from "react";
-import "./index.css";
+import { Menu } from "antd";
+import "./index.less";
+import MenuConfig from "../../config/menuConfig";
+const { SubMenu } = Menu;
 export default class NavLeft extends PureComponent {
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(MenuConfig);
+    this.setState({ menuTreeNode });
+  }
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        );
+      }
+      return <Menu.Item key={item.key}>{item.title}</Menu.Item>;
+    });
+  };
   render() {
-    return <div>NavLeft</div>;
+    return (
+      <div>
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt="" />
+          <h1>ImoocMS</h1>
+        </div>
+        <Menu theme="dark">{this.state.menuTreeNode}</Menu>
+      </div>
+    );
   }
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,21 @@
+export default {
+  formateDate(time) {
+    if (!time) return "";
+    let date = new Date(time);
+    let second =
+      date.getSeconds() > 10 ? date.getSeconds() : "0" + date.getSeconds();
+    return (
+      date.getFullYear() +
+      "年" +
+      (date.getMonth() + 1) +
+      "月" +
+      date.getUTCDate() +
+      "  " +
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      ":" +
+      second
+    );
+  },
+};
